59ccbf71c880ab994ab4ee362f63f8052d58d654
Aliyu Efe <aliyuefe9501@gmail.com>
2023-12-29 00:26:35 +0100

    add form, input and label ui component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,23 @@
 "use client"
-import { Button } from '@/components/ui/button'
-import { Modal } from '@/components/ui/modal'
+
+import { useStoreModal } from "@/hooks/use-store-modal"
+import { useEffect } from "react"
+
+
 
 export default function Home() {
+  const onOpen = useStoreModal(state => state.onOpen)
+  const isOpen = useStoreModal(state => state.isOpen)
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen()
+    }
+  }, [isOpen, onOpen])
+
   return (
     <div className='p-4'>
-      <Modal title="test" description="test desc" isOpen onClose={() => { }}>
-        Children
-      </Modal>
+      Root page
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import ModalProvider from './providers/modal-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
+      <ModalProvider />
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
