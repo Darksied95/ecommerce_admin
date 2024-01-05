be07d8ec870e9dec77797dee63e7a55e8d237104
Aliyu Efe <aliyuefe9501@gmail.com>
2024-01-05 19:40:34 +0100

    update root route

--- src/app/(root)/(routes)/page.tsx
+++ src/app/(root)/(routes)/page.tsx
@@ -1,12 +1,10 @@
 "use client"
 
 import { useStoreModal } from "@/hooks/use-store-modal"
-import { UserButton } from "@clerk/nextjs"
 import { useEffect } from "react"
 
-
-
 export default function Home() {
+
   const onOpen = useStoreModal(state => state.onOpen)
   const isOpen = useStoreModal(state => state.isOpen)
 
@@ -16,10 +14,5 @@ export default function Home() {
     }
   }, [isOpen, onOpen])
 
-  return (
-    <div className='p-4'>
-      Root page
-      <UserButton />
-    </div>
-  )
+  return null
 }
